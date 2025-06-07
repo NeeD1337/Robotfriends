ae1474d4166621c6f2b50c9a7624a51947f6510e
NeeD <itzsergiu@yahoo.com>
2025-06-07 15:45:25 +0300

    first commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,77 @@
+import React,{ Component } from "react";
+import CardList from "../components/CardList.jsx";
+import SearchBox from "../components/SearchBox.jsx";
+import './App.css';
+import Scroll from "../components/Scroll.jsx";
+
+class App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            robots:[],
+            searchfield: ""
+        }
+        // console.log("consturctor");
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+        // console.log("componentDidMount");
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const { robots, searchfield } = this.state; // Destructurare pentru a face codul mai curat
+        const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase()); ///
+        })
+        if(!robots.length) {
+            return <h1 className="tc">Loading...</h1>
+        } else {
+        // console.log("render");
+        return (
+    <div className="tc">
+    <h1 className="f1">RoboFriends</h1>
+    <SearchBox searchChange ={this.onSearchChange}/>
+    <Scroll>
+    <CardList robots = {filteredRobots} />
+    </Scroll>
+    </div>
+    );
+        }
+    }
+}
+
+export default App;
+
+// EXPLICATIE : Cand utilizatorul scrie ceva in SearchBox ul de pe pagina , in fisierul : SearchBox.js avem un input care este :
+// : <input onChange={searchChange} /> , moment in care se declanseaza onChange si se apeleaza functia searchChange de aici din App.jsx adica <SearchBox searchChange ={this.onSearchChange}/>
+// dupa care onSearchChange tot din App.jsx adica de aici , modifica state , mai exact "this.setState" modifica searchfield din state
+// dupa care se detecteaza modificare si se apeleaza din nou render(prima data apelandu se la inceput cand se afiseaza toti robotii pentru ca nu avem nimic scris), intra in actiune filteredRobots care contine doar robotii care se potrivesc cu textul introdus
+// si in final se reafiseaza CardList.jsx doar cu Robotii Potriviti
+
+// props = informații transmise către o componentă de la componenta care o folosește
+// 🔹 1. Componenta:
+// function Buton(props) {
+//   return <button>{props.text}</button>;
+// }
+// 🔹 2. Folosire:
+// <Buton text="Trimite" />
+// <Buton text="Anulează" />
+// 🔹 3. Ce primește componenta:
+// props = {
+//   text: "Trimite"
+// }
+// și apoi:
+// props = {
+//   text: "Anulează"
+// }
+// 🔍 Afișarea finală:
+// <button>Trimite</button>
+// <button>Anulează</button>
+
